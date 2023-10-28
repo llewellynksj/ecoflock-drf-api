d5386d8f8ad6cbda75148001c4c8a394dab647ff
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-10-28 13:40:42 +0000

    Add permissions

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,9 +4,20 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.ReadOnlyField(source='username.username')
+    is_user = serializers.SerializerMethodField()
+
+    def get_is_user(self, obj):
+        request = self.context['request']
+        return request.user == obj.username
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'username', 'created_on', 'first_name', 'last_name', 'profile_pic',
+            'id',
+            'username',
+            'created_on',
+            'first_name',
+            'last_name',
+            'profile_pic',
+            'is_user',
         ]
