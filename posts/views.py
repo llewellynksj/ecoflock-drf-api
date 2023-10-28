8feef80fa456d702a7d417ace1cbf832783390e5
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-10-28 14:34:01 +0000

    Add urls to link up posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+from ecoflock_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class PostsList(APIView):
+    """
+    Gets all posts
+    """
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts,
+            many=True,
+            context={'request': request})
+        return Response(serializer.data)
--- profiles/views.py
+++ profiles/views.py
@@ -42,7 +42,8 @@ class ProfileDetail(APIView):
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
