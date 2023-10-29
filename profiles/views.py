f52337a9c24ef0b6f5ffc9d684ad34ad843255f5
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-10-29 13:11:00 +0000

    Add additional filter to profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from ecoflock_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -16,7 +17,14 @@ class ProfileList(generics.ListAPIView):
         following_count=Count('username__following', distinct=True)
     ).order_by('-created_on')
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        DjangoFilterBackend
+    ]
+    filterset_fields = [
+        # profiles following user
+        'username__following__followed__profile',
+        # profiles followed by user
+        'username__followed__username__profile',
     ]
     ordering_fields = [
         'posts_count',
