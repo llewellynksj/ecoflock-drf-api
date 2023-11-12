4c8b51bcb497355e33682c7a341125e524d7cc12
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-11-12 11:11:03 +0000

    Add upvote and downvote models to voting app

--- voting/models.py
+++ voting/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
+from threads.models import Thread
 
-# Create your models here.
+
+class UpVote(models.Model):
+  """
+  UpVote model
+  Allowers User to up-vote a post or thread
+  """
+  username = models.ForeignKey(User, on_delete=models.CASCADE)
+  post = models.ForeignKey(
+    Post,
+    on_delete=models.CASCADE,
+    blank=True)
+  thread = models.ForeignKey(
+    Thread,
+    on_delete=models.CASCADE,
+    blank=True)
+  created_on = models.DateTimeField(auto_now_add=True)
+
+
+class DownVote(models.Model):
+  """
+  DownVote model
+  Allowers User to down-vote a post or thread
+  """
+  username = models.ForeignKey(User, on_delete=models.CASCADE)
+  post = models.ForeignKey(
+    Post,
+    on_delete=models.CASCADE,
+    blank=True)
+  thread = models.ForeignKey(
+    Thread,
+    on_delete=models.CASCADE,
+    blank=True)
+  created_on = models.DateTimeField(auto_now_add=True)
