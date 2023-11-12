6a6008e9e271742630f297e1d696f3a7c095e9a4
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-11-12 11:33:33 +0000

    Add event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+  """
+  Event model
+  Allows User to create events
+  Allows Users to mark an event as 'interested in'
+  """
+  title = models.CharField(max_length=255)
+  date = models.DateField()
+  time = models.TimeField()
+  description = models.TextField()
+  image = models.ImageField(
+    upload_to='images/',
+    default='../EcoFlock/profile_default.png'
+  )
+  url = models.URLField
+  city = models.CharField(max_length=255)
+  country = models.CharField(max_length=255)
+  cost = models.PositiveIntegerField()
+  users_interested = ArrayField(
+    ArrayField(
+      models.ForeignKey(
+        User, on_delete=models.CASCADE)))
+  created_by = models.ForeignKey(User, on_delete=CASCADE)
+  created_on = models.DateTimeField(auto_now_add=True)
+  updated_on = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    ordering = ['-date']
+
+  def __str__(self):
+    return f"{self.title} - {self.date} - {self.city}"
